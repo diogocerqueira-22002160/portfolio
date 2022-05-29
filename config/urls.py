aea8c476d9abff2c70c5295a41e2e689358d1ef9
diogocerqueira-22002160 <diogoa.cerqueira@gmail.com>
2022-05-29 21:06:47 +0100

    commit inicial

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+# config/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('portfolio.urls')),
+    path('posts/', include('portfolio.urls')),
+]
